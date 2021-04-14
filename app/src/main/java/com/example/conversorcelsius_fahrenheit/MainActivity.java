be3976cc64201b7b9088a8bda127c933dd20e5cd
Duarte Cancela <duartecancela@gmail.com>
2021-04-14 11:02:29 +0100

    Start code

--- app/src/main/java/com/example/conversorcelsius_fahrenheit/MainActivity.java
+++ app/src/main/java/com/example/conversorcelsius_fahrenheit/MainActivity.java
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextInsertTemperature;
+    TextView textViewResult;
+    Button buttonConvert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +20,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    private  void initializeViews() {
+        editTextInsertTemperature = findViewById(R.id.editTextInsertTemperature);
+        textViewResult = findViewById(R.id.textViewResult);
+        buttonConvert = findViewById(R.id.buttonConvert);
+    }
+
     public void onClickTemperatureConvert(View view) {
         System.out.println("Convert");
+
+        // 1. get text from input
+        // 2. Validate data suplied by the user
+        // 2. Convert
+        // 3. Show the result
     }
+
+
 }
