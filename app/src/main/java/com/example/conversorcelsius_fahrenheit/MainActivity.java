3c675e51804539bc95cddbfca7c5145ad36f4508
duartecancela <duartecancela@gmail.com>
2021-04-21 16:08:30 +0100

    F-C and C-F option

--- app/src/main/java/com/example/conversorcelsius_fahrenheit/MainActivity.java
+++ app/src/main/java/com/example/conversorcelsius_fahrenheit/MainActivity.java
@@ -9,11 +9,14 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText editTextInsertTemperature;
     TextView textViewResult;
     Button buttonConvert;
+    EditText editTextSelection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         editTextInsertTemperature = findViewById(R.id.editTextInsertTemperature);
         textViewResult = findViewById(R.id.textViewResult);
         buttonConvert = findViewById(R.id.buttonConvert);
+        editTextSelection = findViewById(R.id.editTextSelection);
     }
 
     public void onClickTemperatureConvert(View view) {
@@ -45,15 +49,36 @@ public class MainActivity extends AppCompatActivity {
         // 3. Convert
         float temperatureToConvert = Float.parseFloat(textFromInput);
         float temperatureInFahrenheit = convertFromCelsiusToFahrenheit(temperatureToConvert);
+        float temperatureInCelsius = convertFahrenheitToCelsius(temperatureToConvert);
 
         // 4. Show the result
-        String result = String.format("%.1f", temperatureInFahrenheit);
-        textViewResult.setText(result);
+        //String resultF = String.format("%.1f", temperatureInFahrenheit);
+        //String resultC = String.format("%.1f", temperatureInCelsius);
+        //textViewResult.setText(resultF);
+
+        // Test type conversion
+        if(editTextSelection.getText().toString().equalsIgnoreCase("C-F")) {
+            Toast.makeText(MainActivity.this, "Resultado de Celsius para Fahrenheit", Toast.LENGTH_SHORT).show();
+            String result = String.format("%.1f", temperatureInFahrenheit);
+            textViewResult.setText(result);
+
+        } else if (editTextSelection.getText().toString().equalsIgnoreCase("F-C")) {
+            Toast.makeText(MainActivity.this, "Resultado de Fahrenheit para Celsius", Toast.LENGTH_SHORT).show();
+            String result = String.format("%.1f", temperatureInFahrenheit);
+            textViewResult.setText(result);
+        } else {
+            Toast.makeText(MainActivity.this, "Coloque F-C ou C-F", Toast.LENGTH_SHORT).show();
+            return;
+        }
     }
 
     private float convertFromCelsiusToFahrenheit(float temperatureInCelsius) {
-        Toast.makeText(MainActivity.this, "Valor convertido!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, "Valor convertido de Celsius para Fahrenheit!", Toast.LENGTH_SHORT).show();
         return temperatureInCelsius * 1.8f + 32f;
     }
 
+    private float convertFahrenheitToCelsius(float temperatureInFahrenheit) {
+        Toast.makeText(MainActivity.this, "Valor convertido de Fahrenheit to Celsius", Toast.LENGTH_SHORT).show();
+        return temperatureInFahrenheit - 32f / 1.8f;
+    }
 }
